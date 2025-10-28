bd6872892cbdacc3f447032d0527e72584a0f11a
michaelfuchaka <fuchakamichael06@gmail.com>
2025-10-28 10:37:45 +0300

    feat: remove Skills component import from Home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 import Hero from "@/components/Home";
 import About from "@/components/About";
 import Services from "@/components/Services";
-import Skills from "@/components/Skills";
+
 import Projects from "@/components/Projects";
 import Testimonials from "@/components/Testimonials";
 import Contact from "@/components/Contact";
